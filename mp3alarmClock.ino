120fa54cff48a52d7038fbcce3faaa67d92e3818
Stefan Kleineberg <stefan.kleineberg@gmx.de>
2025-04-01 22:00:33 +0200

    Abfrage der Tasten über MCP23008 implementiert und funktioniert.

--- mp3alarmClock.ino
+++ mp3alarmClock.ino
@@ -13,17 +13,24 @@
 #include <SD.h>
 #include <SPI.h>
 #include <AudioShieldMinimal.h>
+#include "MCP23008.h"
 
 /******************************************************************************
  * globale Defines
  ******************************************************************************/
 #define SEGCLOCKpin 10
 //#define RELOADvalFor20ms 25536
-#define RELOADvalFor20ms 61535
+#define RELOADvalFor2ms 61535
 #define BCDApin 1
 #define BCDBpin 2
 #define BCDCpin 9
 #define BCDDpin 0
+#define DISPSTATECLK 12 //C
+#define DISPSTATEALM 10 //A
+#define DISPSTATE_TST1 11 //b
+#define DISPSTATE_TST2 13 //d
+#define DISPSTATE_TST3 14 //e
+#define DISPSTATE_TST4 15 //f
 
 
 /******************************************************************************
@@ -39,11 +46,27 @@ unsigned char filenumber = 1;
 //clock variables
 byte currHr = 12;
 byte currMin = 34;
-unsigned int twentyMSecs = 0;
+//alarm variables
+byte alarmHr = 0;
+byte alarmMin = 0;
+//display variables
+byte dispHr = 0;
+byte dispMin = 0;
+byte dispState = 0;
+//unsigned int twentyMSecs = 0;
+unsigned int twoMSecs = 0;
 
 //display variables
 int digitToUpdate = 0;
 
+//i2c-port-extender
+MCP23008 MCP(0x20);
+#define ALMBUTTON     0
+#define SETTIMEBUTTON 1
+#define HOURINCBUTTON 2
+#define MININCBUTTON  3
+#define ALMONBUTTON   4
+byte buttonState = 0;
 
 /******************************************************************************
  * Funktionen
@@ -120,7 +143,7 @@ void setup()
   TCCR1A = 0;
   TCCR1B = 0;
   //Laden des Zaehlerwertes
-  TCNT1 = RELOADvalFor20ms;
+  TCNT1 = RELOADvalFor2ms;
   //Prescaler: 8
   TCCR1B |= (1 << CS11);
   //Enable timer overflow interrupt
@@ -139,18 +162,30 @@ void setup()
 
   //
   Serial.end();
-  
+
   //Alle Interrupts aktivieren
   interrupts();
+  
+  Wire.begin();
+  MCP.begin();
+
+  while(!(MCP.isConnected()))
+  {
+    //wait to be connected
+    //verbindet aber nicht
+  }
+  
+  MCP.pinMode8(0xFF);
 }
 
 
 ISR(TIMER1_OVF_vect)
 {
-  //1 minute passed, 50 occurrences of 20ms = 1sec and 60 of them means 1 min
-  if (twentyMSecs == (50 * 60))
+  //START EXT-PIN-COUNT-IRQ
+  //1 minute passed, 500 occurrences of 2ms = 1sec and 60 of them means 1 min
+  if (twoMSecs == (500 * 60))
   {
-    twentyMSecs = 0;
+    twoMSecs = 0;
     if (currMin < 59)
     {
       currMin++;
@@ -168,48 +203,97 @@ ISR(TIMER1_OVF_vect)
       }
     }
   }
+  //END EXT-PIN-COUNT-IRQ
   //Pin-Wert an Portpin schreiben
   digitalWrite(SEGCLOCKpin, LOW);
   if (digitToUpdate == 0)
   {
-    writeBcdToSegPins(currHr, 1);
+    writeBcdToSegPins(dispHr, 1);
     digitToUpdate = 1;
   }
   else if (digitToUpdate == 1)
   {
-    writeBcdToSegPins(currHr, 0);
+    writeBcdToSegPins(dispHr, 0);
     digitToUpdate = 2;
   }
   else if (digitToUpdate == 2)
   {
-    writeBcdToSegPins(currMin, 1);
+    writeBcdToSegPins(dispMin, 1);
     digitToUpdate = 3;
   }
   else if (digitToUpdate == 3)
   {
-    writeBcdToSegPins(currMin, 0);
+    writeBcdToSegPins(dispMin, 0);
     digitToUpdate = 4;
   }
   else if (digitToUpdate == 4)
   {
-    writeBcdToSegPins(12, 2);
+    writeBcdToSegPins(dispState, 2);
     digitToUpdate = 5;
   }
   else if (digitToUpdate == 5)
   {
     //
-    writeBcdToSegPins(12, 2);
+    writeBcdToSegPins(dispState, 2);
     digitToUpdate = 0;
   }
   //Pin-Wert an Portpin schreiben
   digitalWrite(SEGCLOCKpin, HIGH);
   //
-  twentyMSecs++;
+  twoMSecs++;
   //Laden des Zaehlerwertes
-  TCNT1 = RELOADvalFor20ms;
+  TCNT1 = RELOADvalFor2ms;
 }
 
 void loop()
 {
-  delay( 1000 );
+  /*
+  if (MCP.read1(ALMBUTTON))
+  {
+    buttonState = buttonState | (1 << ALMBUTTON);
+    dispState = DISPSTATECLK;
+  }
+  else
+  {
+    buttonState = buttonState & B11111110;
+  }
+  if (MCP.read1(SETTIMEBUTTON))
+  {
+    buttonState = buttonState | (1 << SETTIMEBUTTON);
+    dispState = DISPSTATEALM;
+  }
+  else
+  {
+    buttonState = buttonState & B11111101;
+  }
+  if (MCP.read1(HOURINCBUTTON))
+  {
+    buttonState = buttonState | (1 << HOURINCBUTTON);
+  }
+  else
+  {
+    buttonState = buttonState & B11111011;
+  }
+  if (MCP.read1(MININCBUTTON))
+  {
+    buttonState = buttonState | (1 << MININCBUTTON);
+  }
+  else
+  {
+    buttonState = buttonState & B11110111;
+  }
+  */
+  if (MCP.read1(ALMONBUTTON) == false)
+  {
+    dispHr = currHr;
+    dispMin = currMin;
+    dispState = DISPSTATEALM;
+  }
+  else
+  {
+    dispHr = currHr;
+    dispMin = currMin;
+    dispState = DISPSTATECLK;
+  }
+  delay(100);
 }
